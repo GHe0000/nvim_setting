ba1a1f9b9c768e43efca348fe5b1c7257cf7a3c3
Guotao He <guotao.he.0000@outlook.com>
2025-01-03 22:06:41 +0800

    Update

--- init.lua
+++ init.lua
@@ -7,18 +7,18 @@ vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
 -- Windows 下默认终端设置为 Powershell
-local powershell_options = {
-	shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-	shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-	shellquote = "",
-	shellxquote = "",
-}
+-- local powershell_options = {
+-- 	shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+-- 	shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+-- 	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+-- 	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+-- 	shellquote = "",
+-- 	shellxquote = "",
+-- }
 
-for option, value in pairs(powershell_options) do
-	vim.opt[option] = value
-end
+-- for option, value in pairs(powershell_options) do
+-- 	vim.opt[option] = value
+-- end
 
 require("options")
 require("keymaps")
